2a07fa9dcda331ba3fd4c9cfa929e0b9e2b46751
Alexander <kemperomg@gmail.com>
2013-11-04 06:10:48 +0200

    Start to create Controls constructor
    Start to create control's constructor, for slides by month

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-var CalendarBuilder = function(options) {
+'use strict';
+var CalendarBuilder = function (options) {
 	// main options
 	this.options = options;
 	this.options.current_date = new Date();
@@ -7,12 +8,16 @@ var CalendarBuilder = function(options) {
 	this.options.current_month = this.options.current_date.getMonth();
 	this.options.current_year = this.options.current_date.getFullYear();
 
-	// hardcode data
-	this.options.month_array = options.month_array || ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-	this.options.days_array = options.days_array || ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+	// hardcoded data
+	this.options.month_array = options.month_array || ['January', 'February',
+		'March', 'April', 'May', 'June', 'July', 'August', 'September',
+		'October', 'November', 'December'];
+	this.options.days_array = options.days_array || ['Monday', 'Tuesday',
+		'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 	// for monthly build
-	this.options.new_date = new Date(this.options.current_year, this.options.current_month);
+	this.options.new_date = new Date(
+		this.options.current_year, this.options.current_month);
 	this.options.new_date_day_of_week = this.options.new_date.getDay();
 	this.options.new_date_day = this.options.new_date.getDate();
 
@@ -21,7 +26,7 @@ var CalendarBuilder = function(options) {
 	this.options.td_array = [];
 	this.options.last_day_of_month = 0;
 
-	// hardcode rows
+	// hardcoded rows
 	// and cells
 	this.calendar_rows = 6;
 	this.calendar_cells = 7;
@@ -29,12 +34,13 @@ var CalendarBuilder = function(options) {
 	this.init();
 };
 
-CalendarBuilder.prototype.create_help_lib = function() {
+CalendarBuilder.prototype.create_help_lib = function () {
+
 	var self = this;
 
 	CalendarBuilder.prototype.lib = {};
 
-	CalendarBuilder.prototype.lib.get_real_day = function(date) {
+	CalendarBuilder.prototype.lib.get_real_day = function (date) {
 		var day = date.getDay() - 1,
 			real_day;
 
@@ -50,11 +56,11 @@ CalendarBuilder.prototype.create_help_lib = function() {
 	};
 };
 
-CalendarBuilder.prototype.build_calendar_layout = function() {
+CalendarBuilder.prototype.build_calendar_layout = function () {
 	var frg = document.createDocumentFragment(),
 		table = document.createElement('table'),
 		tbody = document.createElement('tbody'),
-		thead = table.createTHead(),// document.createElement('thead'),
+		thead = table.createTHead(), // document.createElement('thead'),
 		tfoot = table.createTFoot(),
 
 		tr_array = this.options.tr_array,
@@ -74,13 +80,13 @@ CalendarBuilder.prototype.build_calendar_layout = function() {
 	table.appendChild(tbody);
 	frg.appendChild(table);
 
-	function add_row(tbody, index) {
+	function add_row (tbody, index) {
 		// var new_tr = document.createElement('tr');
 		// tr_array.push(tbody.appendChild(new_tr));
 		tr_array.push(tbody.insertRow(-1));
 	}
 
-	function add_cell(tr, index) {
+	function add_cell (tr, index) {
 		// var new_td = document.createElement('td');
 		// td_array.push(tr.appendChild(new_td));
 		td_array.push(tr.insertCell(-1));
@@ -92,7 +98,7 @@ CalendarBuilder.prototype.build_calendar_layout = function() {
 		add_row(tbody, i);
 
 		// build cells
-		for ( j = 0 ; j < this.calendar_cells; j += 1 ) {
+		for ( j = 0; j < this.calendar_cells; j += 1 ) {
 			add_cell(tr_array[i], j);
 		}
 	}
@@ -100,8 +106,9 @@ CalendarBuilder.prototype.build_calendar_layout = function() {
 	this.options.container.appendChild(frg);
 };
 
-// TODO make it better
-CalendarBuilder.prototype.fill_cells = function() {
+//TODO make it better
+CalendarBuilder.prototype.fill_cells = function () {
+
 	var real_calendar_day = this.lib.get_real_day(this.options.new_date);
 
 	this.options.first_day = this.options.td_array[real_calendar_day];
@@ -110,8 +117,8 @@ CalendarBuilder.prototype.fill_cells = function() {
 
 	// console.log(this.options.new_date.getDay());
 
-	while (this.options.new_date.getDate() !== 1) {
-		if ( this.options.new_date.getDate() === this.options.current_day ) {
+	while ( this.options.new_date.getDate() !== 1 ) {
+		if (this.options.new_date.getDate() === this.options.current_day) {
 			this.options.today = this.options.td_array[real_calendar_day + 1];
 		}
 		real_calendar_day += 1;
@@ -121,23 +128,24 @@ CalendarBuilder.prototype.fill_cells = function() {
 	}
 };
 
-CalendarBuilder.prototype.set_month_label = function() {
+CalendarBuilder.prototype.set_month_label = function () {
 	this.options.caption = this.options.table.createCaption();
-	this.options.caption.innerHTML = this.options.month_array[this.options.current_month];
+	this.options.caption.innerHTML =
+		this.options.month_array[this.options.current_month];
 };
 
-CalendarBuilder.prototype.set_cell_html = function(index) {
-	if ( this.options.new_date_day_of_week() === index ) {
+CalendarBuilder.prototype.set_cell_html = function (index) {
+	if (this.options.new_date_day_of_week() === index) {
 		this.options.last_day_of_month += 1;
 	}
 };
 
-CalendarBuilder.prototype.highlight_control_days = function() {
+CalendarBuilder.prototype.highlight_control_days = function () {
 	this.options.first_day.className = 'first';
 	this.options.today.className = 'today';
 };
 
-CalendarBuilder.prototype.set_week_days = function() {
+CalendarBuilder.prototype.set_week_days = function () {
 	var tr = this.options.thead.insertRow(-1),
 		th,
 		i = 0;
@@ -149,7 +157,7 @@ CalendarBuilder.prototype.set_week_days = function() {
 	}
 };
 
-CalendarBuilder.prototype.init = function() {
+CalendarBuilder.prototype.init = function () {
 	this.create_help_lib();
 	this.build_calendar_layout();
 	this.fill_cells();
@@ -158,6 +166,38 @@ CalendarBuilder.prototype.init = function() {
 	this.set_week_days();
 };
 
+/*
+*
+* Calendar's controls constructor
+*
+* */
+
+var CalendarControls = function (options) {
+	// store options
+	this.options = options || {};
+
+	// container to insert controls
+	this.containerToInsert = options.containerToInsert;
+
+	// main control items settings
+	this.nextOptions = options.nextOptions || {};
+	this.prevOptions = options.prevOptions || {};
+
+	// create prev and next buttons
+	this.next = this.createControl('next', this.nextOptions);
+	this.prev = this.createControl('prev', this.prevOptions);
+};
+
+CalendarControls.prototype.createControl = function (options) {
+	var holder, element;
+
+	holder = document.createDocumentFragment();
+	element = document.createElement(options.elementType || 'a');
+};
+
 var calendar = new CalendarBuilder({
 	container: document.getElementById('container')
+});
+var controls = new CalendarControls({
+	containerToInsert : calendar.container
 });
