8220e8ec4078448993cc7d83a96ca8c5e73af712
Alexander <kemperomg@gmail.com>
2015-01-25 19:30:54 +0100

    List all days within this month

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
 (function () {
   'use strict';
 
-  var SCOPE = this;
+  var SCOPE     = this,
+    currentDate = new Date();
 
   function Calendar (options) {
+    this.options = options;
 
+    /* List every date day from start till end of this month */
+    for (var dateDay = 1; dateDay <= currentDate.getDate(); dateDay += 1) {
+      this.options.container.innerHTML += ' ' + dateDay;
+    }
+
+    var _currentDayDate = currentDate.getDate();
+
+    currentDate.setDate(dateDay);
+
+    while (currentDate.getDate() > _currentDayDate) {
+      this.options.container.innerHTML += ' ' + dateDay;
+      dateDay += 1;
+      currentDate.setDate(dateDay);
+    }
   }
+
+  SCOPE.__calendar = new Calendar({
+    container : SCOPE.container
+  });
 
 }).call(this);
